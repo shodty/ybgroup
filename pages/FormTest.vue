b79ed59d3c5824117f170ad92ce1e56e5ad2b8aa
shodty <rob.landsburg@gmail.com>
2020-01-16 02:55:40 -0800

    form testing

---
+++ pages/FormTest.vue
@@ -0,0 +1,38 @@
+<template>
+  <form
+    name="ask-question"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+  >
+    <input type="hidden" name="form-name" value="ask-question">
+    <label v-for="(panelist, index) in panelists" :key="index">
+      <input
+        :value="panelist"
+        @input="ev => updatePanelist"
+        :checked="panelist === currentPanelist"
+        type="radio"
+        name="panelist"
+      >
+      <span>{{ panelist }}</span>
+    </label>
+    ...
+    <button>Submit</button>
+  </form>
+</template>
+<script>
+export default {
+  name: 'QAForm',
+  data () {
+    return {
+      panelists: ['Evan You', 'Chris Fritz'],
+      currentPanelist: 'Evan You'
+    }
+  },
+  methods: {
+    updatePanelist (ev) {
+      this.currentPanelist = ev.target.value
+    }
+  }
+}
+</script>
