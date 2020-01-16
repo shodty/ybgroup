2125c68493381212270a5ebd879e670c406ca993
shodty <rob.landsburg@gmail.com>
2020-01-16 02:50:10 -0800

    added names to form inputs

--- pages/MailService.vue
+++ pages/MailService.vue
@@ -6,28 +6,28 @@
   DarkLight
   b-row(align-h="center")
     b-col(cols=5 md=5)
-      b-form(name='ybg-mail-service' method='post' id="mailserviceform" @submit="onSubmit" data-netlify="true" data-netlify-honeypot="bot-field")
+      b-form(name='ybg-mail-service' method='POST' id="mailserviceform" data-netlify="true" data-netlify-honeypot="bot-field")
         input(type='hidden' name='form-name' value='ask-question')
         b-form-row( id="input-group-1"  label-for="input-1" )
             b-col(cols="3" class="blackborder" )
                 p First Name
             b-col(class="nopadding")
-                b-form-input(id="input-1" v-model="form.firstname" type="text" required placeholder="Enter first name")
+                b-form-input(id="input-1" name="firstname" type="text" required placeholder="Enter first name")
         b-form-row( id="input-group-2"  label-for="input-2" )
             b-col(cols="3" class="blackborder")
                 p Last Name
             b-col(class="nopadding")
-                b-form-input(id="input-2" v-model="form.lastname" type="text" required placeholder="Enter last name")
+                b-form-input(id="input-2" name="lastname" type="text" required placeholder="Enter last name")
         b-form-row( id="input-group-3"  label-for="input-3" )
             b-col(cols="3" class="blackborder")
                 p Email
             b-col(class="nopadding")
-                b-form-input(id="input-3" v-model="form.email" type="email" required placeholder="Enter email")
+                b-form-input(id="input-3" name="email" type="email" required placeholder="Enter email")
         b-form-row( id="input-group-4"  label-for="input-4" )
             b-col(cols="3" class="blackborder")
                 p Mailing Address
             b-col(class="nopadding")
-                b-form-input(id="input-4" v-model="form.address" type="text" required placeholder="Enter mailing address")
+                b-form-input(id="input-4" name="mailingaddress" type="text" required placeholder="Enter mailing address")
         b-form-row
             b-col(cols="10")
             b-col(cols="2" class="nopadding")
