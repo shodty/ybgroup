c5a58a46c8eceab316eb963b3f2fbb899e6bbad8
shodty <rob.landsburg@gmail.com>
2020-01-17 12:23:00 -0800

    fixed click box

---
+++ components/IconThree.vue
@@ -0,0 +1,91 @@
+<template lang="pug">
+.svgContainer
+  svg(xmlns='http://www.w3.org/2000/svg' :width='width' :height='height' viewBox='0 0 400 400' :aria-labelledby='iconName' role='presentation' :class="rotate? 'rotating' : null")
+    title(:id='iconName' lang='en') {{iconName}}
+    g(:fill='iconColor' class='transitional' :stroke='iconStroke' :stroke-width='strokeWeight')
+      slot
+</template>
+
+<script>
+export default {
+  props: {
+    iconName: {
+      type: String,
+      default: ''
+    },
+    width: {
+      type: [Number, String],
+      default: '22vw'
+    },
+    height: {
+      type: [Number, String],
+      default: '22vw'
+    },
+    iconColor: {
+      type: String,
+      default: 'currentColor'
+    },
+    iconStroke: {
+      type: String,
+      default: 'rgb(0,0,0,0)'
+    },
+    strokeWeight: {
+      type: Number,
+      default: 0
+    },
+    rotate: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+svg
+  display: inline-block
+  //vertical-align: baseline
+  //margin-bottom: -2px
+
+.transitional
+  transition: all .8s ease
+
+.svgContainer
+  margin 0 auto
+  width: inherit
+  height: inherit
+
+@keyframes rotating {
+    from {
+        transform: rotate(0deg);
+        -o-transform: rotate(0deg);
+        -ms-transform: rotate(0deg);
+        -moz-transform: rotate(0deg);
+        -webkit-transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+        -o-transform: rotate(360deg);
+        -ms-transform: rotate(360deg);
+        -moz-transform: rotate(360deg);
+        -webkit-transform: rotate(360deg);
+    }
+}
+@-webkit-keyframes rotating {
+    from {
+        transform: rotate(0deg);
+        -webkit-transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+        -webkit-transform: rotate(360deg);
+    }
+}
+.rotating {
+    -webkit-animation: rotating 9s linear infinite;
+    -moz-animation: rotating 9s linear infinite;
+    -ms-animation: rotating 9s linear infinite;
+    -o-animation: rotating 9s linear infinite;
+    animation: rotating 9s linear infinite;
+}
+</style>
--- components/MailLogo.vue
+++ components/MailLogo.vue
@@ -1,19 +1,20 @@
 <template lang="pug">
 #header-div
     .headerimages
-        .icon-container( @click='onClick')
-            IconBase(icon-name="yellow brick group" width="15vh" height="15vh" :icon-color=' light? "black" : "white" ' rotate=true)
+        .icon-container(@click='onClick')
+            IconThree(icon-name="yellow brick group" width="100%" height="100%" :icon-color=' light? "black" : "white" ' rotate=true)
               component(:is="componentName[this.count]" )
+            .click-box
 </template>
 
 <script>
 
-import IconBase from './IconBase.vue'
+import IconThree from './IconThree.vue'
 
 export default {
   name: 'AppHeader',
   components: {
-    IconBase
+    IconThree
   },
   data () {
     return {
@@ -39,11 +40,23 @@ export default {
 
 <style scoped lang="stylus">
 #header-div
+  width 10%
   position: relative
   z-index : 900
   margin: 0 auto
   text-align: center
-
+  @media(max-width: 767px){
+    width 25%
+  }
+  @media(min-width: 768px){
+    width 15%
+  }
+  @media(min-width: 1001px){
+    width 12%
+  }
+  @media(min-width: 1200px){
+    width 8%
+  }
 .icon-container
   position: relative
   padding-top: 1vh
@@ -51,5 +64,15 @@ export default {
   @media(max-width: 767px){
       padding-top: 0
   }
+
+.click-box
+  position: absolute
+  display: block
+  height: 100%
+  width: 10%
+  margin: 0 auto
+  top: 0
+  left: 45%
+  cursor: url("../assets/eye.png"), auto
 
 </style>
